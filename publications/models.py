dc6406800b8f56a6d9f27cf5326744a07e7553b9
andre <andre@b2e9302d-b4d2-0310-80c4-8586bce995ec>
2007-03-20 23:01:17 +0000

    Created locale support
    git-svn-id: file:///home/andreanjos/svn/andre_website/trunk@14 b2e9302d-b4d2-0310-80c4-8586bce995ec

--- publications/models.py
+++ publications/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 from django.db import models
@@ -9,13 +10,18 @@ class Publication(models.Model):
   proceedings.
   """
   
-  title = models.CharField('Title of the publication', maxlength=256)
-  date = models.DateField('Publishing date')
-  author_list = models.TextField('Author names')
-  media = models.CharField('Journal or proceedings', maxlength=256)
-  pub_type = models.CharField('Type of publication', maxlength=64)
-  abstract = models.TextField('A summary of the work')
+  title = models.CharField(_('Title of the publication'), maxlength=256)
+  date = models.DateField(_('Publishing date'))
+  author_list = models.TextField(_('Author names'))
+  media = models.CharField(_('Journal or proceedings'), maxlength=256)
+  pub_type = models.CharField(_('Type of publication'), maxlength=64)
+  abstract = models.TextField(_('A summary of the work'))
   files = models.ManyToManyField(File, filter_interface=models.HORIZONTAL)
+
+  # make it translatable
+  class Meta:
+    verbose_name = _('publication')
+    verbose_name_plural = _('publications')
 
   # make it admin'able
   class Admin:
