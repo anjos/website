beaed37de2648c6ffda329853114f682df08de6b
andre <andre@b2e9302d-b4d2-0310-80c4-8586bce995ec>
2007-03-21 22:28:43 +0000

    Deploy generic views
    git-svn-id: file:///home/andreanjos/svn/andre_website/trunk@21 b2e9302d-b4d2-0310-80c4-8586bce995ec

--- publications/urls.py
+++ publications/urls.py
@@ -1,5 +1,26 @@
 from django.conf.urls.defaults import *
+from django.views.generic import list_detail
+from publications.models import Publication
 
-urlpatterns = patterns('stuff.publications.views',
-                       (r'^$', 'index'),
-                       (r'^(?P<pub_id>\d+)/$', 'get'))
+from settings import MEDIA_URL
+from django.contrib.sites.models import Site
+
+# urlpatterns = patterns('stuff.publications.views',
+#                        (r'^$', 'index'),
+#                        (r'^(?P<pub_id>\d+)/$', 'get'))
+
+publication_list = { 'queryset': Publication.objects.all(),
+                     'extra_context': {'site_name': Site.objects.filter(id=1)[0].name,
+                                       }
+                     }
+
+publication_detail = { 'queryset': Publication.objects.filter(),
+                       'extra_context': {'site_name': Site.objects.filter(id=1)[0].name,
+                                         'media_url': MEDIA_URL,
+                                         }
+                       }
+
+urlpatterns = patterns('',
+                       (r'^$', list_detail.object_list, publication_list),
+                       (r'^(?P<object_id>\d+)/$', list_detail.object_detail, publication_detail),
+                       )
