f343fca1f037da8a9af80fe27befa9a30b243da2
andre <andre@b2e9302d-b4d2-0310-80c4-8586bce995ec>
2007-03-24 23:50:58 +0000

    More translations
    git-svn-id: file:///home/andreanjos/svn/andre_website/trunk@36 b2e9302d-b4d2-0310-80c4-8586bce995ec

--- files/feeds.py
+++ files/feeds.py
@@ -3,12 +3,13 @@ from django.utils.feedgenerator import Atom1Feed
 from django.contrib.sites.models import Site
 from files.models import File
 from settings import MEDIA_URL
+from django.utils.translation import gettext_lazy as _
 
 class TenLastFilesUploaded(Feed):
     feed_type = Atom1Feed
-    title = "Files at %s" % Site.objects.filter(id=1)[0].domain
+    title = _("Files at %s") % Site.objects.filter(id=1)[0].domain
     link = "/"
-    description = "The last public uploads to the site"
+    description = _("The last public uploads to the site")
     title_template = "feeds/files_title.html"
     description_template = "feeds/files_description.html"
 
--- publications/feeds.py
+++ publications/feeds.py
@@ -3,13 +3,14 @@ from django.utils.feedgenerator import Atom1Feed
 from django.contrib.sites.models import Site
 from publications.models import Publication
 from settings import MEDIA_URL
+from django.utils.translation import gettext_lazy as _
 
 class TenLastPublications(Feed):
     feed_type = Atom1Feed
     #title = unicode("Publications at " + Site.objects.filter(id=1)[0].name, 'utf-8').encode('ascii', 'xmlcharrefreplace')
-    title = "Publications at " + Site.objects.filter(id=1)[0].domain
+    title = _("Publications at ") + Site.objects.filter(id=1)[0].domain
     link = "/publication/"
-    description = "The last things I wrote"
+    description = _("The last things I wrote")
     title_template = "feeds/publications_title.html"
     description_template = "feeds/publications_description.html"
 
