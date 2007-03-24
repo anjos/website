cead3047553062e59541917d418aa25981902048
andre <andre@b2e9302d-b4d2-0310-80c4-8586bce995ec>
2007-03-24 08:26:57 +0000

    Made the setup a bit smarter
    git-svn-id: file:///home/andreanjos/svn/andre_website/trunk@33 b2e9302d-b4d2-0310-80c4-8586bce995ec

--- setup.sh
+++ setup.sh
@@ -1,7 +1,17 @@
 # This script is meant to be sourced
+# You should make sure the DJANGO installment
+# is available on the path bellow or the
+# script will abort
+
+# Andre Anjos <andre.dos.anjos@gmail.com>
+# Original from 24th of March 2007
 
 DJANGO=${HOME}/tmp/django;
 
-export PATH=${DJANGO}/django/bin:${PATH};
-export PYTHONPATH=${DJANGO}:${PYTHONPATH};
+if [ ! -d ${DJANGO} ]; then
+  echo "The directory ${DJANGO} is not available. Check your installation!";
+else
+  export PATH=${DJANGO}/django/bin:${PATH};
+  export PYTHONPATH=${DJANGO}:${PYTHONPATH};
+fi
 
