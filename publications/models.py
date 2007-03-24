e0a8c619d92df639d19c99589837fed3d0dd6125
andre <andre@b2e9302d-b4d2-0310-80c4-8586bce995ec>
2007-03-24 08:27:49 +0000

    More fields in publication; Make files and abstract optional at DB and admin interface
    git-svn-id: file:///home/andreanjos/svn/andre_website/trunk@34 b2e9302d-b4d2-0310-80c4-8586bce995ec

--- publications/models.py
+++ publications/models.py
@@ -14,9 +14,17 @@ class Publication(models.Model):
   date = models.DateField(_('Publishing date'))
   author_list = models.TextField(_('Author names'))
   media = models.CharField(_('Journal or proceedings'), maxlength=256)
+  number = models.TextField(_('Number'), null=True, blank=True, maxlength=16)
+  volume = models.TextField(_('Volume'), null=True, blank=True, maxlength=16)
+  pages = models.TextField(_('Pages'), null=True, blank=True, maxlength=16)
   pub_type = models.CharField(_('Type of publication'), maxlength=64)
-  abstract = models.TextField(_('A summary of the work'))
-  files = models.ManyToManyField(File, filter_interface=models.HORIZONTAL)
+  abstract = models.TextField(_('A summary of the work'), blank=True)
+  files = models.ManyToManyField(File, filter_interface=models.HORIZONTAL,
+                                 null=True, blank=True)
+
+  def count_files(self):
+    """Counts the number of files attached to this publication."""
+    return len(self.files.all())
 
   # make it translatable
   class Meta:
@@ -25,10 +33,11 @@ class Publication(models.Model):
 
   # make it admin'able
   class Admin:
-    list_display = ('title', 'media', 'date')
+    list_display = ('title', 'media', 'date', 'count_files')
     list_filter = ['date']
+    ordering = ['-date']
     search_fields = ['title', 'media', 'date']
     date_hierarchy = 'date'
     
   def __str__(self):
-    return '%s (%s)' % (self.title, self.date)
+    return '%s (%s)' % (self.title, self.date.strftime('%B %Y'))
