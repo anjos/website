f822b94d990902021967258da23a6c559b255754
andre <andre@b2e9302d-b4d2-0310-80c4-8586bce995ec>
2008-04-11 16:14:18 +0000

    New installation script
    git-svn-id: file:///home/andreanjos/svn/andre_website/trunk@79 b2e9302d-b4d2-0310-80c4-8586bce995ec

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash 
+# Created by Andre Anjos <andre.dos.anjos@cern.ch>
+# Sex 11 Abr 2008 14:39:42 CEST
+
+if [ $# = 0 ]; then
+  echo "usage: $0 python-executable-name"
+  exit 1
+fi
+
+function replace () {
+  #1. what, 
+  #2. newvalue, 
+  #3. file
+  echo "Changing file $3...";
+  sed -i -e "s#^$1\(\s*\)=\(\s*\).\+#$1\1=\2$2#" $3
+}
+
+for f in bootstrap.sh Makefile; do
+  cp $f $f~
+  replace PYTHON python2.5 $f
+  replace BASEDIR $PWD $f
+done
+
+for f in stuff/Makefile; do
+  cp $f $f~
+  replace BASEDIR $PWD/stuff $f
+done
+
+for f in stuff/settings.py; do
+  cp $f $f~
+  replace BASEDIR "'$PWD/stuff'" $f
+  replace DATABASE "'$PWD/db.sql3'" $f
+  replace MEDIA_ROOT "'$PWD/media'" $f
+done
+
