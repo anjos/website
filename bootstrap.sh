02404c8f196387c2285e48729b1622b368e7c156
andre <andre@b2e9302d-b4d2-0310-80c4-8586bce995ec>
2008-09-15 15:25:49 +0000

    Greatly simplified bootstrap procedure
    git-svn-id: file:///home/andreanjos/svn/andre_website/trunk@106 b2e9302d-b4d2-0310-80c4-8586bce995ec

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,124 +4,49 @@
 
 # Automatically set!
 BASEDIR=`pwd`
-PYTHON=python
-
-# This script will download and install all necessary software for us
-[ ! -d sw/installed ] && mkdir -pv sw/installed;
-
-# A few environment setups
-python_version=`${PYTHON} -c 'import sys; print "python%d.%d" % sys.version_info[0:2]'`
-export PYTHONPATH=`pwd`/sw/installed/lib/${python_version}/site-packages
+PYTHON=python2.4
+INSTALLDIR=${BASEDIR}/sw/installed
+python_version=`${PYTHON} -c 'import sys;print "%d.%d" % sys.version_info[0:2]'`
 
 # Versions
-scons_version=0.98.5;
-gdata_version=1.1.1;
-etree_version=1.2.6-20050316;
-feedparser_version=4.1;
-pydelicious_version=0.5.0;
-textile_version=2.0.11;
-pysqlite2_version=2.4.1;
+setuptools_egg=setuptools-0.6c8-py${python_version}.egg
+setuptools=http://pypi.python.org/packages/${python_version}/s/setuptools/${setuptools_egg};
+docutils=http://docutils.sourceforge.net/docutils-snapshot.tgz
+django=http://www.djangoproject.com/download/1.0/tarball/;
+scons=http://prdownloads.sourceforge.net/scons/scons-1.0.1.tar.gz;
+gdata=http://gdata-python-client.googlecode.com/files/gdata.py-1.2.0.tar.gz;
+etree=http://effbot.org/media/downloads/elementtree-1.2.6-20050316.tar.gz
+feedparser=http://feedparser.googlecode.com/files/feedparser-4.1.zip
+pydelicious=http://pydelicious.googlecode.com/files/pydelicious-0.5.0.zip
+textile=http://pypi.python.org/packages/source/t/textile/textile-2.0.11.tar.gz
+pysqlite2=http://oss.itsystementwicklung.de/download/pysqlite/2.5/2.5.0/pysqlite-2.5.0.tar.gz
 
-function target() {
-  echo "Retrieving `basename $1`..."
-  wget --quiet $1;
-  tar xfz `basename $1`;
-}
-function zipget() {
-  echo "Retrieving `basename $1`..."
-  wget --quiet $1;
-  unzip -qq `basename $1`;
-}
 
-function setup() {
-  echo "Setting up $1..."
-  cd $1;
-  ${PYTHON} setup.py --quiet build;
-  ${PYTHON} setup.py --quiet install --prefix=${BASEDIR}/sw/installed;
-  cd -;
-}
+# This script will download and install all necessary software for us
+[ ! -d ${INSTALLDIR} ] && mkdir -pv ${INSTALLDIR}
 
-function svnup() {
-  echo "Updating (svn) $1...";
-  cd $1;
-  svn --quiet update .;
-  cd -;
+export PYTHONPATH=${INSTALLDIR}
+# A few environment setups
+# First we install the setuptools
+echo "### Installing ${setuptools_egg}..."
+wget ${setuptools} 
+sh ${setuptools_egg} --install-dir=${INSTALLDIR}
+rm -f ${setuptools_egg}
+echo "### Installation of ${setuptools_egg} is done!"
+
+function install () {
+  echo "### Installing $1..."
+  ${PYTHON} ${INSTALLDIR}/easy_install-${python_version} --install-dir=${INSTALLDIR} $2;
+  echo "### Installation of $1 is done!"
 }
 
-# change to sw dir
-pushd sw
+install docutils ${docutils}
+install django ${django}
+install scons ${scons}
+install gdata ${gdata}
+install elementTree ${etree}
+install feedparser ${feedparser}
+install pydelicious ${pydelicious}
+install textile ${textile}
+install pysqlite2 ${pysqlite2}
 
-# scons download and installation
-if [ ! -d scons-${scons_version} ]; then
-  target http://prdownloads.sourceforge.net/scons/scons-${scons_version}.tar.gz
-fi
-setup scons-${scons_version};
-
-# django trunk
-if [ ! -d django-trunk ]; then
-  svn --quiet co http://code.djangoproject.com/svn/django/trunk django-trunk
-else
-  svnup django-trunk
-fi
-setup django-trunk
-rm -f ../media/django;
-ln -s ${BASEDIR}/sw/installed/lib/${python_version}/site-packages/django/contrib/admin/media ../media/django
-
-# gdata
-if [ ! -d gdata.py-${gdata_version} ]; then
-  target http://gdata-python-client.googlecode.com/files/gdata.py-${gdata_version}.tar.gz
-fi
-setup gdata.py-${gdata_version} 
-
-# element tree
-if [ ! -d elementtree-${etree_version} ]; then
-  target http://effbot.org/media/downloads/elementtree-${etree_version}.tar.gz
-fi
-setup elementtree-${etree_version}
-
-# flup
-if [ ! -d flup-trunk ]; then
-  svn --quiet co http://svn.saddi.com/flup/trunk flup-trunk
-else
-  svnup flup-trunk
-fi
-setup flup-trunk 
-
-# docutils trunk
-if [ ! -d docutils-trunk ]; then
-  svn --quiet co http://svn.berlios.de/svnroot/repos/docutils/trunk/docutils docutils-trunk
-else
-  svnup docutils-trunk;
-fi
-setup docutils-trunk
-
-# feedparser (we need special code for this one)
-if [ ! -d feedparser-${feedparser_version} ]; then
-  echo "Installing feedparser..."
-  wget --quiet http://feedparser.googlecode.com/files/feedparser-${feedparser_version}.zip
-  mkdir feedparser-${feedparser_version};
-  cd feedparser-${feedparser_version};
-  unzip -qq ../feedparser-${feedparser_version}.zip;
-  cd -;
-fi
-setup feedparser-${feedparser_version}
-
-# pydelicious trunk
-if [ ! -d pydelicious-${pydelicious_version} ]; then
-  zipget http://pydelicious.googlecode.com/files/pydelicious-${pydelicious_version}.zip
-fi
-setup pydelicious-${pydelicious_version}
-
-# textile 
-if [ ! -d textile-${textile_version} ]; then
-  target http://pytextile.googlecode.com/files/textile-${textile_version}.tar.gz
-fi
-setup textile-${textile_version}
-
-# pysqlite 2
-if [ ! -d pysqlite-${pysqlite2_version} ]; then
-  target http://oss.itsystementwicklung.de/download/pysqlite/2.4/2.4.1/pysqlite-2.4.1.tar.gz
-fi
-setup pysqlite-${pysqlite2_version}
-
-popd
