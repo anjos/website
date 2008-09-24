82ac9f578c805811b1444319ef8c9c9646c52db8
andre <andre@b2e9302d-b4d2-0310-80c4-8586bce995ec>
2008-09-24 06:58:40 +0000

    More robust passenger startup script
    git-svn-id: file:///home/andreanjos/svn/andre_website/trunk@141 b2e9302d-b4d2-0310-80c4-8586bce995ec

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -4,21 +4,28 @@
 
 import sys, os
 
-INTERP = "/usr/bin/python2.4"
-if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
+try:
+  INTERP = "/usr/bin/python2.4"
+  if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
 
-base = '/home/andreanjos/andre.adois.org'
+  base = '/home/andreanjos/andre.adois.org'
 
-for f in os.listdir(base + '/sw'):
-  egg = os.sep.join([base + '/sw', f])
-  if f[-4:] == '.egg': sys.path.insert(0, egg)
+  for f in os.listdir(base + '/sw'):
+    egg = os.sep.join([base + '/sw', f])
+    if f[-4:] == '.egg': sys.path.insert(0, egg)
 
-# Two extras for Django
-sys.path.insert(0, base + '/stuff')
-sys.path.insert(0, base)
+  # Two extras for Django
+  sys.path.insert(0, base + '/stuff')
+  sys.path.insert(0, base)
 
-os.environ['BASEDIR']=os.path.realpath(base)
-os.environ['DJANGO_SETTINGS_MODULE']='stuff.settings'
+  os.environ['BASEDIR']=os.path.realpath(base)
+  os.environ['DJANGO_SETTINGS_MODULE']='stuff.settings'
 
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+  import django.core.handlers.wsgi
+  application = django.core.handlers.wsgi.WSGIHandler()
+
+except Exception, e:
+  
+  def application(environ, start_response):
+    write = start_response('200 OK', [('Content-type', 'text/plain')])
+    return ["Exception caught!\n** Here is the output:\n%s\n** Your sys.path is:\n%s" % (e, '\n'.join(sys.path))]
