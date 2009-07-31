424bd65e4a753a1da922f6560644c7aa8750064c
André Anjos <rabello@pcuw32.cern.ch>
2009-07-31 14:20:59 +0200

    Extend library path, define python

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 # Created by Andre Anjos <andre.dos.anjos@cern.ch>
 # Sex 08 Ago 2008 15:35:48 CEST
 
-export PATH=`pwd`/sw:${PATH}
-export PYTHONPATH=`pwd`:`pwd`/sw:${PYTHONPATH}
+export PATH=`pwd`/sw:${HOME}/sw/bin:${PATH}
+export PYTHONPATH=`pwd`:`pwd`/sw:`pwd`/sw/PIL/lib/python:${PYTHONPATH}
 export BASEDIR=`pwd`
+export PYTHON=`which python2.5`
