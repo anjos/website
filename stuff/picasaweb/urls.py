7cf6f603e97eba42750529fec10c1aac727e4c6a
André Anjos <rabello@pcuw32.cern.ch>
2009-08-04 17:40:48 +0200

    Removed old picasaweb bridge

--- stuff/picasaweb/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('',
-                       (r'^$', 'picasaweb.views.view_gallery'),
-                       (r'^(?P<id>\d+)/$', 'picasaweb.views.view_gallery'),
-                       )
-
