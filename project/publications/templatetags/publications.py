b763bbc7966d8f63742e249ad2980d365cc7cceb
André Anjos <rabello@pcuw32.cern.ch>
2010-02-17 17:39:15 +0100

    Lots of modifications for a two-column start page.
    * Simplified stylesheets
    * Developed new bubble system for publications
    * Uniformized download buttons
    * Streamlined css inclusion
    * Fixed stylesheets

--- project/publications/templatetags/publications.py
+++ project/publications/templatetags/publications.py
@@ -8,15 +8,22 @@
 
 from django import template
 register = template.Library()
+from django.conf import settings
 
 from project.publications.models import Publication
 
 # This will return the media necessary for publications
-def publications_media(context): return context
-register.inclusion_tag('publications/embed/media.html', takes_context=True)(publications_media)
+@register.inclusion_tag('publications/embed/media_widgets.html')
+def publications_widgets_media(url=settings.MEDIA_URL):
+  return {'MEDIA_URL': url}
+
+@register.inclusion_tag('publications/embed/media.html')
+def publications_media(url=settings.MEDIA_URL):
+  return {'MEDIA_URL': url}
 
 @register.inclusion_tag('publications/embed/last_bubble.html')
-def last_publications_bubble(n):
+def last_publications_bubble(n, include_header):
   """Puts out a bubble with the latest 'n' publications."""
-  return {'objects': Publication.objects.order_by('-date')[:n]}
+  return {'objects': Publication.objects.order_by('-date')[:n],
+          'include_header': include_header}
 
